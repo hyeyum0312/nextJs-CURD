6ee927bb35571ec1466908baaceeffe2ab426b96
조혜연 <hyeyum0312@gmail.com>
2024-03-29 01:11:47 +0900

    feat: comment리스트 만들기

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,22 +1,48 @@
-'use client'
+'use client' 
+//DB출력문법 작성불가, 서버에게 DB출력 결과요청 가능.
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Comment(props) {
-    
     let [comment, setComment] = useState('');
-    
+    let [commentList, setCommentList] = useState([]);
+        console.log('props.data._id',props.data._id);
+      useEffect(()=>{
+        // ajax,timer등등..동작.
+        // 재랜더링 될 때마다 실행됩니다.
+        //첫 로드 1회만 실행을 원할경우 []를 추가합니다.
+        // html보여준 후 늦게 동작해야합니다.
+
+        fetch('/api/comment/list?id=' + props.data._id).then(r=>r.json()).then((result)=>{
+          console.log('result',result)
+          setCommentList(result);
+        })
+      },[]) 
 
     return (
         <div>
-        <div>댓글목록</div>
-        <input onChange={(e)=>{
-            setComment(e.target.value);
-        }}/>
+            <hr/>
+            <ul>
+                {
+                    commentList.length > 0 ? 
+                    commentList?.map((item,index)=>{
+                        return (<li key={index}>{item.content} - {item.author} </li>)
+                    })
+                    : '작성된 댓글이 없습니다'
+                }
+            </ul>
+            <input onChange={(e)=>{
+                setComment(e.target.value);
+            }}/>
 
-        <button onClick={()=>{
-            fetch('/api/comment/new', {method: 'POST', body: JSON.Stringfy({comment : comment, _id: props.data._id})})
-        }}>댓글전송</button>
+            <button onClick={()=>{
+                fetch('/api/comment/new', {method: 'POST', body: JSON.stringify({comment : comment, _id: props.data._id})}).then(
+                    fetch('/api/comment/list?id=' + props.data._id).then(r=>r.json()).then((result)=>{
+                        console.log('result',result)
+                        setCommentList(result);
+                      })
+                )
+            }}>댓글전송</button>
         </div>
     )
 }
--- pages/api/comment/new.ts
+++ pages/api/comment/new.ts
@@ -1,17 +1,23 @@
 import { connectDB } from "@/app/util/database";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 export default async function handler(req:any, res:any){
-
+     let session = await getServerSession(req,res,authOptions);
+    console.log('session',session);
     if (req.method === "POST") {
         try{
             // 페이지 이동 
             const db = (await connectDB).db("forum");
+            req.body = JSON.parse(req.body);
+            console.log('eq.body',req.body);
+            
             let save = {
                 content: req.body.comment,
-                parent: req.body._id,
-                author: '작성자 이메일'
+                parent: new ObjectId(req.body._id),
+                author: session?.user?.email
             }
-            let result = await db.collection("post").insertOne(req.body)
+            let result = await db.collection("comment").insertOne(save);
             
             return res.status(200);
         } catch(error) {
